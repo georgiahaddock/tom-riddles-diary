a126cd8a49616fbc85e2d94b89494a64e7dc119e
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2023-02-09 15:09:51 +0000

    GET /diaryentries route added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const {DiaryEntry} = require(./models/DiaryEntry.js);
+const {DiaryEntry} = require('./models/DiaryEntry.js');
 const app = express();
 const port = 3000;
+const sequelize = require('./db');
 
-
+app.get('/diaryentries', async(req, res, next) => {
+    const diaryEntries = await DiaryEntry.findAll();
+    res.send(diaryEntries);
+})
