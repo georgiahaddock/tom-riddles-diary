11950b0b6d6813c7406a180e8e43a3b91e294b09
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2023-02-21 16:57:28 +0000

    get /diaryentries route added

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const {DiaryEntry} = require('./models/DiaryEntry.js');
 const app = express();
 const port = 3000;
-const sequelize = require('./db/db');
+const { sequelize } = require('./db/db');
 
 app.get('/diaryentries', async(req, res, next) => {
     try{
         const diaryEntries = await DiaryEntry.findAll();
 
         if(diaryEntries){
-            res.sendStatus(200).send(diaryEntries);
+            res.send(diaryEntries);
             next();
         }else{
             res.send("Entries not found! There must be an error somewhere.")
@@ -21,5 +21,6 @@ app.get('/diaryentries', async(req, res, next) => {
 });
 
 app.listen(port, () => {
+    sequelize.sync({ force: false });
     console.log(`listening on port http://localhost:${port}/diaryentries`);
 })
