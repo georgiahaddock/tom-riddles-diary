f5ee0b8882adb3fde2ef097968fc2d3249b3df44
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2023-02-22 11:24:33 +0000

    post /diaryentries route created

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const { sequelize } = require('./db/db');
 
+app.use(express.json());
+
 app.get('/diaryentries', async(req, res, next) => {
     try{
         const diaryEntries = await DiaryEntry.findAll();
@@ -19,6 +21,23 @@ app.get('/diaryentries', async(req, res, next) => {
         next(err);
     }
 });
+
+app.post('/diaryentries', async (req, res, next) => {
+    try{
+        console.log(req.body);
+        let { newEntry } = req.body;
+        if(!newEntry.title || !newEntry.passage){
+            throw new Error('invalid request body');
+        }else{
+            newEntry = await DiaryEntry.create(newEntry);
+            console.log('passage created!')
+            res.send(newEntry);
+        }
+    }catch(err){
+        res.send(err);
+        next(err)
+    }
+})
 
 app.listen(port, () => {
     sequelize.sync({ force: false });
