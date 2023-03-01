f8902a4299d21dec2c79f1ec819dda262285d8d3
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2023-03-01 13:05:46 +0000

    http over https

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET,
-  baseURL: 'https://localhost:3000',
+  baseURL: 'http://localhost:3000',
   clientID: 'tkCxwh91d3dF5VFr7qQ08MMsYsMkJCKz',
   issuerBaseURL: 'https://dev-r20xhvfyn8f1kdg3.uk.auth0.com'
 };
