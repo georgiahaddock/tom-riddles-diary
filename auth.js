302cf8a7d122ede939ffdef3bf71d2ad71e1ded9
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2023-03-01 12:18:45 +0000

    auth secret made secure using .env file

--- auth.js
+++ auth.js
@@ -1,9 +1,10 @@
 const { auth } = require('express-openid-connect');
+require('dotenv').config();
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
+  secret: process.env.SECRET,
   baseURL: 'https://localhost:3000',
   clientID: 'tkCxwh91d3dF5VFr7qQ08MMsYsMkJCKz',
   issuerBaseURL: 'https://dev-r20xhvfyn8f1kdg3.uk.auth0.com'
